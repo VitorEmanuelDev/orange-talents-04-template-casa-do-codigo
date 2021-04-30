38d2d9aa26bd9df611329fdbf98deb8a913ef747
root <root@ZUP-6253.localdomain>
2021-04-30 16:30:33 -0300

    Lista de livros cadastrados. Versão 10

--- casa_do_codigo/src/main/java/com/casa_do_codigo/livro/LivroController.java
+++ casa_do_codigo/src/main/java/com/casa_do_codigo/livro/LivroController.java
@@ -6,9 +6,10 @@ import javax.persistence.*;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 
@@ -36,6 +37,20 @@ public class LivroController {
 	public ResponseEntity<List<LivroModel>> getAll(){ 
 		return ResponseEntity.ok(livroRepository.findAll());
 	}
-
+	
+	
+	@GetMapping(value = "/livros/{id}")
+	public DetalheLivroResponse getById(@PathVariable Long id){ 
+		
+		LivroModel livro = manager.find(LivroModel.class, id);
+		
+		if(livro == null){
+			
+	           throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+	           
+	        }
+		
+	        return new DetalheLivroResponse(livro);
+	}
 
 }
